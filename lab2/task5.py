cbe6a8cc7edddffa1de7855017ef5178e93dc7c4
Andrii Prykhodko <58827069+andriiprykhodko96@users.noreply.github.com>
2023-11-17 23:55:36 +0200

    Create task5.py

---
+++ lab2/task5.py
@@ -0,0 +1,27 @@
+import hazelcast
+import threading
+from time import time
+
+lock = threading.Lock()
+
+def increment(map, key):
+    for _ in range(10000):
+        value = count.add_and_get(1)
+        map.put(key, value)
+
+
+if name == 'main':
+    hz = hazelcast.HazelcastClient()
+    map = hz.get_map("my-dist-map").blocking()
+    key = "task_5"
+    count = hz.cp_subsystem.get_atomic_long(key).blocking()
+    map.put(key,0)
+    start=time()
+    threads =[threading.Thread(target=increment,
+                               args=[map, key]) for _ in range(10)]
+    for thread in threads:
+        thread.start()
+    for thread in threads:
+        thread.join()
+    print("Time: %s seconds" % (time() - start))
+    print(map.get("task_5"))
